2c10ebc47426639d012b51aba0b1fab541a3cd3a
neemkashu <sparjinkust@mail.ru>
2022-10-13 22:38:16 +0300

    feat: add vigenere-cipher. feat: init what-season

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,91 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor (isDirect = true) {
+        if (isDirect) {
+            this.machinetype = 'direct';
+        } else {
+            this.machinetype = 'inverse';
+        }
+    }
+    encrypt(messageStr, keyStr) {
+        if ((arguments.length < 2)  ||
+            (messageStr === undefined) ||
+            (keyStr === undefined)
+        )  {
+            throw new Error('Incorrect arguments!');
+        }
+
+        const message = messageStr.toUpperCase().split('');
+        const key = keyStr.toUpperCase().split('');
+        
+        let j = 0;
+        for (let i = 0; i < message.length; i++) {
+            if (! isCapLetter(message[i])) continue;
+
+            message[i] = encryptChar(message[i], key[j]);
+            j = (j + 1) % key.length;
+
+        }
+        if (this.machinetype === 'inverse') message.reverse();
+
+        return message.join('');
+
+        function encryptChar(M, K){
+            const m = M.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0);
+            const k = K.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0);
+            
+            const c = (m + k) % 26; // wikipedia formula
+            return String.fromCharCode(c + 'A'.charCodeAt(0));
+        }
+
+        function isCapLetter (char) {
+            const code = char.charCodeAt(0) - 'A'.charCodeAt(0);
+            return ((code >= 0) && (code < 26));
+        }
+    }
+    decrypt(ciphredStr, keyStr) {
+        if ((arguments.length < 2)  ||
+            (ciphredStr === undefined) ||
+            (keyStr === undefined)
+        ) {
+            throw new Error('Incorrect arguments!');
+        }
+
+        const ciphred = ciphredStr.toUpperCase().split('');
+        const key = keyStr.toUpperCase().split('');
+
+        let j = 0;
+        for (let i = 0; i < ciphred.length; i++) {
+            if (! isCapLetter(ciphred[i])) continue;
+
+            ciphred[i] = decryptChar(ciphred[i], key[j]);
+            j = (j + 1) % key.length;
+
+        }
+        if (this.machinetype === 'inverse') ciphred.reverse();
+
+        return ciphred.join('');
+
+        function decryptChar(C, K){
+            const c = C.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0);
+            const k = K.toUpperCase().charCodeAt(0) - 'A'.charCodeAt(0);
+            
+            const m = ((c - k) > 0? c - k : c - k + 26) % 26; // wikipedia formula
+            return String.fromCharCode(m + 'A'.charCodeAt(0));
+        }
+
+        function isCapLetter (char) {
+            const code = char.charCodeAt(0) - 'A'.charCodeAt(0);
+            return ((code >= 0) && (code < 26));
+        }
+        
+    }
 }
+const directMachine = new VigenereCipheringMachine();
+console.debug(directMachine.decrypt('UWJJW XAGWLNFM VNNNDXHVWWL :)', 'js'));
+// 'AEIHQX SX DLLU!';
+
 
 module.exports = {
   VigenereCipheringMachine
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) { 
+    let myDate = new Date;
+    if (date === undefined) return 'Unable to determine the time of year!';
+    if (Object.getPrototypeOf(date) !== Object.getPrototypeOf(myDate)) throw new Error('Invalid date!');
+    if (! isDate(date) ) throw new Error('Invalid date!');
+
+
+    const month = date.getMonth();
+    if (month < 2 || month == 11) return 'winter';
+    if (month > 1 && month <=4) return 'spring';
+    if (month > 4 && month <= 7) return 'summer';
+    return 'autumn';
+
+    function isDate (date) {
+        let myDate = new Date;
+        if ( ! (date instanceof Date)) return false;
+        if (date.__proto__ !== Date.prototype) return false;
+        if (date.constructor !== Date) return false;
+        if ( ! (Object.prototype.toString.call(date) === "[object Date]") ) return false;
+        if (Object.prototype.valueOf(myDate) !== Object.prototype.valueOf(date) ) return false;
+        return true;
+    }
 }
 
 module.exports = {
