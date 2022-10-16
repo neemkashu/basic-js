2698973927337b781f9f3a93c4d199e0fc1265dd
neemkashu <sparjinkust@mail.ru>
2022-10-16 17:49:10 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater( str, options) {
+    options = optionsChecker(options);
+    if (typeof str !== 'string') str = String(str);
+    if (typeof options.addition !== 'string') options.addition = String(options.addition);
 
+    let addCell = new Array(options.additionRepeatTimes);
+
+    const additionTotal = addCell.fill('').map( (element) => element = options.addition).join(options.additionSeparator);
+
+    let strCell = new Array(options.repeatTimes);
+    const strTotal = strCell.fill('').map( (element) => element = str + additionTotal).join(options.separator);
+
+    return strTotal;
+
+    function optionsChecker(options) {
+        if (!('repeatTimes' in options)) options.repeatTimes = 1;
+        if (!('separator' in options)) options.separator = '+';
+        if (!('addition' in options)) options.addition = '';
+        if (!('additionRepeatTimes' in options)) options.additionRepeatTimes = 1;
+        if (!('additionSeparator' in options)) options.additionSeparator = '|';
+        return options;        
+    }
+}
+let str = repeater(9.234, { repeatTimes: 4, separator: '||', addition: { a: 5 }, additionRepeatTimes: 3, additionSeparator: '&&' });
+console.log(str);
 module.exports = {
   repeater
 };
